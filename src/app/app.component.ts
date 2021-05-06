67b79ea74fa2992c0648ad1bee43b59a210e7e66
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-06 17:54:36 +0100

    top navbar working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  page = "sign-in"
+  current_page = "sign-in"
+  changePage(event: string){
+    this.current_page = event;
+  }
 }
--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-nav.component.css']
 })
 export class TopNavComponent implements OnInit {
-
+  @Output() current_page_emitter = new EventEmitter<string>();
   constructor() { }
-
   ngOnInit(): void {
   }
 
