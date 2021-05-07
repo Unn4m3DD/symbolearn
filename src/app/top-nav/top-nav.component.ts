b9ccc07591fe47528f79d6e036677d2dfb329ad5
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-07 21:15:32 +0100

    bubbles and some attempts to on learn morse

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -6,14 +6,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./top-nav.component.css']
 })
 export class TopNavComponent implements OnInit {
-  @Output() current_page_emitter = new EventEmitter<string>();
   constructor() { 
   }
   ngOnInit(): void {
   }
   changePage(page_name: string){
-    console.log(page_name)
-    this.current_page_emitter.emit(page_name)
     window.location.pathname = "/" + page_name
   }
 
