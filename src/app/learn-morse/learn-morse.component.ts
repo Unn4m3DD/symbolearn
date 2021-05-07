db1cc4bdc79b92471ebdb8c45ceeebebefcc6248
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-07 10:33:40 +0100

    learn morse functional

--- src/app/learn-morse/learn-morse.component.ts
+++ src/app/learn-morse/learn-morse.component.ts
@@ -6,15 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./learn-morse.component.css']
 })
 export class LearnMorseComponent implements OnInit {
+  correct_counter: number = 0;
+  encodeMorse(morseCode: string): string {
+    let ref: { [key: string]: string } = {
+      'a': '.-',
+      'b': '-...',
+      'c': '-.-.',
+      'd': '-..',
+      'e': '.',
+      'f': '..-.',
+      'g': '--.',
+      'h': '....',
+      'i': '..',
+      'j': '.---',
+      'k': '-.-',
+      'l': '.-..',
+      'm': '--',
+      'n': '-.',
+      'o': '---',
+      'p': '.--.',
+      'q': '--.-',
+      'r': '.-.',
+      's': '...',
+      't': '-',
+      'u': '..-',
+      'v': '...-',
+      'w': '.--',
+      'x': '-..-',
+      'y': '-.--',
+      'z': '--..',
+      '1': '.----',
+      '2': '..---',
+      '3': '...--',
+      '4': '....-',
+      '5': '.....',
+      '6': '-....',
+      '7': '--...',
+      '8': '---..',
+      '9': '----.',
+      '0': '-----',
+      ' ': '\n'
+    };
+    return ref[morseCode.toLowerCase()];
+  }
 
   user_input: string = ""
-  constructor() { }
+  to_type_render: { char: string, index: number }[][] = []
+  to_type = "Hello World"
+  constructor() {
+    let g_index = 0;
+    for (let i = 0; i < this.to_type.split(" ").length; i++) {
+      this.to_type_render.push([])
+      g_index++
+      for (let j = 0; j < this.to_type.split(" ")[i].length; j++) {
+        this.to_type_render[i].push({ char: this.to_type.split(" ")[i].charAt(j), index: g_index })
+        g_index++
+      }
+    }
+  }
   onUserInput(event: KeyboardEvent) {
     if (event.key.length == 1) {
       this.user_input += event.key;
+      if (event.key == this.to_type.charAt(this.correct_counter).toLowerCase()) this.correct_counter++
       if (this.user_input.length > 20)
         this.user_input = this.user_input.substring(1);
     }
+    console.log(this.correct_counter)
   }
   ngOnInit(): void {
   }
