33176b12b45b7e39d27a7dbeae08eaa5616bcd14
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-17 01:42:43 +0000

    add new language screen

--- src/app/add-language-final/add-language-final.component.ts
+++ src/app/add-language-final/add-language-final.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
@@ -7,11 +7,13 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
   styleUrls: ['./add-language-final.component.css']
 })
 export class AddLanguageFinalComponent implements OnInit {
-  to_render: { character: string, image: string | SafeUrl }[];
+  @ViewChild('file_input') file_input: ElementRef | undefined;
+  to_render: { character: string, image: string | SafeUrl, hovering: number }[];
+  current_char : string = "";
   constructor(private sanitizer: DomSanitizer) {
     this.to_render = []
     for (let i of "abcdefghijklmnopqrstuvwxyz".toUpperCase().split(""))
-      this.to_render.push({ character: i, image: "" })
+      this.to_render.push({ character: i, image: "", hovering: 0 })
 
   }
 
@@ -25,10 +27,26 @@ export class AddLanguageFinalComponent implements OnInit {
 
   parseFile(char: string, event: any) {
     const item = this.to_render.find(e => e.character == char)
-    if (item)
+    if (item) {
       item.image =
         this.sanitize((window.URL ? URL : webkitURL).createObjectURL(event.dataTransfer.files[0]))
-
+      item.hovering = 0
+    }
+  }
+  selectedFile(event: any){
+    this.parseFile(this.current_char, {dataTransfer: event.target})
+  }
+  selectFile(char: string) {
+    this.current_char = char
+    this.file_input?.nativeElement.click()
+  }
+  setHovering(char: string, value: boolean) {
+    const item = this.to_render.find(e => e.character == char)
+    if (item)
+      if (value)
+        item.hovering++
+      else
+        item.hovering--
   }
   ngOnInit(): void {
   }
