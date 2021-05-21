822f8db07c2ac5da2a4d26769b1701b3048c84e6
tiagocas <tiago.rainho@ua.pt>
2021-05-21 22:54:58 +0100

    finish exercises component and animation added

--- src/app/exercises/exercises.component.ts
+++ src/app/exercises/exercises.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercises',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercises.component.css']
 })
 export class ExercisesComponent implements OnInit {
+  @ViewChild('slider') slider: ElementRef | any;
+  @ViewChild('right_arrow') right_arrow: ElementRef | any;
+  @ViewChild('left_arrow') left_arrow: ElementRef | any;
+  public cards_buffer: number = 0;
+
   exercises = [{
     id: "01",
     title: "SymboLearn",
@@ -25,12 +30,58 @@ export class ExercisesComponent implements OnInit {
       wpm: 156,
       accuracy: "99%"
     }
+  },
+  {
+    id: "02",
+    title: "Hamlet",
+    subtitle: "Hamlet's first act",
+    developer: "Baianinho de Mauá",
+    stats: {
+      wpm: 156,
+      accuracy: "99%"
+    }
+  },
+  {
+    id: "02",
+    title: "Hamlet",
+    subtitle: "Hamlet's first act",
+    developer: "Baianinho de Mauá",
+    stats: {
+      wpm: 156,
+      accuracy: "99%"
+    }
+  },
+  {
+    id: "02",
+    title: "Hamlet",
+    subtitle: "Hamlet's first act",
+    developer: "Baianinho de Mauá",
+    stats: {
+      wpm: 156,
+      accuracy: "99%"
+    }
   }
 ]
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+    this.left_arrow.nativeElement.style.display = 'none';
+    if(this.exercises.length <= 4) this.right_arrow.nativeElement.style.display = 'none';
+  }
+
   ngOnInit(): void {
+  }
+
+  slide(direction: string) {
+    const deltaX = 300 * 1;
+    if (direction == "right") this.cards_buffer += deltaX;
+    else if (direction == "left") this.cards_buffer -= deltaX;
+    if(this.cards_buffer == 0) this.left_arrow.nativeElement.style.display = 'none';
+    else this.left_arrow.nativeElement.style.display = 'inline';
+    if(this.cards_buffer >= deltaX * (this.exercises.length - 4)) this.right_arrow.nativeElement.style.display = 'none';
+    else this.right_arrow.nativeElement.style.display = 'inline';
+    this.slider.nativeElement.style.transform = `translate(-${this.cards_buffer}px, 0)`;
   }
 
 }
